554a0d952238ce91027ddec2bbfe89ec976b6f39
Stanislav <kaggy8@ya.ru>
2022-10-23 18:31:44 +0300

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Stanislav Demyanov on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
