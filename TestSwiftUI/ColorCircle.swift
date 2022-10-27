daec58a4dd5fc06341d5733d551d464f3e45225c
Stanislav <kaggy8@ya.ru>
2022-10-28 01:14:01 +0300

    Refactoring

--- TestSwiftUI/ColorCircle.swift
+++ TestSwiftUI/ColorCircle.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ColorCircle: View {
     let color: Color
+    let opacity: Double
     
     var body: some View {
         Circle()
             .foregroundColor(color)
             .frame(width: 150, height: 150)
             .overlay(Circle().stroke(Color.white, lineWidth: 8))
+            .opacity(opacity)
     }
 }
 
 struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ColorCircle(color: .red)
+        ColorCircle(color: .red, opacity: 0.25)
     }
 }
--- TestSwiftUI/ContentView.swift
+++ TestSwiftUI/ContentView.swift
@@ -8,26 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var buttonTitle = "START"
-    @State private var numberOfPressed = 0
     @State private var opacityValueRed = 0.25
     @State private var opacityValueYellow = 0.25
     @State private var opacityValueGreen = 0.25
+    @State private var numberOfPressed = 0
     
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             VStack {
-                ColorCircle(color: .red)
-                    .opacity(opacityValueRed)
-                    .padding(.top, 20)
-                ColorCircle(color: .yellow)
-                    .padding(.top, 20)
-                    .opacity(opacityValueYellow)
-                ColorCircle(color: .green)
-                    .padding(.top, 20)
-                    .padding(.bottom, 150)
-                    .opacity(opacityValueGreen)
+                ColorCircle(color: .red,
+                            opacity: opacityValueRed)
+                .padding(.top, 20)
+                ColorCircle(color: .yellow,
+                            opacity: opacityValueYellow)
+                .padding(.top, 20)
+                ColorCircle(color: .green,
+                            opacity: opacityValueGreen)
+                .padding(.top, 20)
+                .padding(.bottom, 150)
                 
                 buttonLight
             }
@@ -37,35 +38,46 @@ struct ContentView: View {
     private var buttonLight: some View {
         Button(action: {
             buttonTitle = "NEXT"
-            
-            if numberOfPressed == 0 {
-                opacityValueRed = 1
-                opacityValueGreen = 0.25
-                numberOfPressed += 1
-            } else if numberOfPressed == 1 {
-                opacityValueRed = 0.25
-                opacityValueYellow = 1
-                numberOfPressed += 1
-            } else {
-                opacityValueYellow = 0.25
-                opacityValueGreen = 1
-                numberOfPressed = 0
-            }
-        }) {
+            changeColor()
+        })
+        {
             Text(buttonTitle)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .multilineTextAlignment(.center)
                 .frame(width: 150, height: 50)
-                .background(Color.blue
-                    .cornerRadius(25))
                 .foregroundColor(.white)
+        }
+        .background(Color.blue
+            .cornerRadius(25))
+        .overlay {
+            RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
+                .stroke(Color.white, lineWidth: 8)
         }
     }
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+        }
+    }
+    
+    private func changeColor() {
+        switch numberOfPressed {
+        case 0:
+            opacityValueRed = 1
+            opacityValueGreen = 0.25
+            numberOfPressed += 1
+        case 1:
+            opacityValueRed = 0.25
+            opacityValueYellow = 1
+            numberOfPressed += 1
+        case 2:
+            opacityValueYellow = 0.25
+            opacityValueGreen = 1
+            numberOfPressed = 0
+        default:
+            break
         }
     }
 }
